5ab88d66e4d7229c908e5d773a29bfc535a434aa
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-02-21 13:00:58 +0100

    feat: js file modified

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+/*
+
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina la concatenazione delle variabili + un numero a caso scelto da voi
+
+*/
+
+const userName = prompt('Qual è il tuo nome?')
+const userSurname = prompt('Qual è il tuo cognome?')
+
+const favColour = prompt('Qual è l tuo colore preferito?')
+
+// document.writeln(userName + userSurname + favColour + 21)
+
+document.writeln(`${userName}${userSurname}${favColour}33`)
